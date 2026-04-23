3b67ddce5b4ff2c226ba37ea5a6b783efda10531
logyQT <stoltmann96@gmail.com>
2026-04-23 18:40:51 +0200

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 import createNextIntlPlugin from "next-intl/plugin";
 
-// Wskazujemy ścieżkę do pliku, który stworzyliśmy przed chwilą
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 /** @type {import('next').NextConfig} */
---
+++ src/lib/swagger.ts
@@ -0,0 +1,31 @@
+import { registry } from "@/lib/zod";
+import { OpenApiGeneratorV3 } from "@asteasolutions/zod-to-openapi";
+import { createSwaggerSpec } from "next-swagger-doc";
+import {} from "@/server/models";
+
+export const getApiDocs = async () => {
+  const generator = new OpenApiGeneratorV3(registry.definitions);
+  const zodComponents = generator.generateComponents();
+  const spec = createSwaggerSpec({
+    apiFolder: "src/app/api",
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "StudiQ API Documentation",
+        version: "1.0.0",
+      },
+      components: {
+        ...(zodComponents.components as any),
+        securitySchemes: {
+          BearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      },
+      security: [],
+    },
+  });
+  return spec;
+};
